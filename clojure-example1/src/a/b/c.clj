ee8c24b20e75dfeea105636d8549c1707d4b1aac
Johnson Yang <johnson_yang2014@163.com>
2017-07-09 10:15:17 +0800

    Closure Practice

--- clojure-example1/src/a/b/c.clj
+++ clojure-example1/src/a/b/c.clj
@@ -1,6 +1,46 @@
 (ns a.b.c)
-(defn hello-world [username]
-  (println (format "Hello, %s" username)))
+(ns str
+  (:use [clojure.string]))
 
-(hello-world "world2")
+;(ns io (:use [clojure.java.io]))
+
+(require '[clojure.java.io :as io])
+
+
+(defn hello-world [username]
+  (println (format "Hello, %s" username))
+  )
+
+;(def data-file (resource "f://tmp//a1.txt"))
+;(slurp data-file)
+
+(def str2 (str "abc" "/ABC/" "/456"))
+(def str1 (str "abc" "/123/" str2))
+(hello-world str1)
+
+
+; You can call java methods such as File/exists
+(if (.exists (io/as-file "dummy.txt"))(println "Y")(println "N"))
+(if (> 5 2)(println "YES")(println "NO"))
+
+
+;;file函数，获取一个java.io.File对象
+;(def f (file "f://tmp//a1.txt"))
+
+;;拷贝文件使用copy
+;(copy f (file "f://tmp//b1.txt"))
+
+;;删除文件，使用delete-file
+;(delete-file f)
+
+;;更经常使用reader和writer
+;(def rdr (reader "b.txt" :encoding "utf-8"))
+;(def wtr (writer "c.txt" :append true))
+
+;;copy可以接受多种类型的参数
+;(copy rdr wtr :buffer-size 4096)
+
+;;关闭文件
+;(.close wtr)
+;(.close rdr)
 
---
+++ clojure-example1/src/a/b/d.clj
@@ -0,0 +1,4 @@
+(ns a.b.d)
+(def x (+ 3 4))
+(defn y [m] (println (* m x)))
+(y 10)
